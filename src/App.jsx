ac83a56f49a489d332e19a7e03c9eed3ec477a1f
RafaelCastrillonEndo0 <castrillonrafael7@gmail.com>
2024-11-12 14:37:29 -0500

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import FormSignin from "./components/FormSignin";
-import HomeUser from  "./Pages/HomeUser"
 import Navbar from "./components/Navbar"
 import FormSignup from "./components/FormSignup";
+import { ThemeProvider } from "./components/theme-provider";
 import { auth } from "./firebase/firebaseconfig";
 import { getFirestore, doc, getDoc, collection, getDocs } from 'firebase/firestore';
 
@@ -15,19 +15,28 @@ function App() {
   const fireStore = getFirestore();
 
   useEffect(() => {
+    const savedUserDetails = localStorage.getItem("userDetails");
+    if (savedUserDetails) {
+      setUserDetails(JSON.parse(savedUserDetails)); 
+    }
+
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
       if (user) {
         setUser(user);
         const docRef = doc(fireStore, "usuarios", user.uid);
         const docSnapShot = await getDoc(docRef);
         if (docSnapShot.exists()) {
-          setUserDetails(docSnapShot.data());
+          const userDetails = docSnapShot.data();
+          setUserDetails(userDetails);
+          // Guardar en localStorage
+          localStorage.setItem("userDetails", JSON.stringify(userDetails));
         } else {
           console.log("No such document!");
         }
       } else {
         setUser(null);
         setUserDetails(null);
+        localStorage.removeItem("userDetails"); 
       }
     });
     return () => unsubscribe();
@@ -51,18 +60,16 @@ function App() {
     fetchUsers();
   }, [fireStore]);
 
-
-
   return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <BrowserRouter>
-      
       <Routes>
-        <Route path="/home" element={<Navbar user={userDetails}/>}>
-        </Route>
-        <Route path="/signin" element={user ? <Navigate to={"/home"} /> : <FormSignin />} />
+        <Route path="/home" element={<Navbar user={userDetails}/>}></Route>
+        <Route path="/signin" element={<FormSignin />} />
         <Route path="/signup" element={<FormSignup />} />
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,109 @@
 import { Outlet } from "react-router-dom";
 import Home from "../Pages/HomeUser";
 import "./Navbar.css";
-
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+    DropdownMenuGroup,
+} from "@/components/ui/dropdown-menu";
+import {
+    House,
+    Cloud,
+    CreditCard,
+    Github,
+    Keyboard,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    MessageSquare,
+    Plus,
+    PlusCircle,
+    Settings,
+    User,
+    UserPlus,
+    Users,
+    School,
+    Presentation,
+  } from "lucide-react"
 function Navbar({ user }) {
-    console.log(user)
-  return (
-    <div className="navbar-container">
-      <div className="Navbar">
-        <img className="logo" src="/src/img/logo.png" alt="" />
-        <h2 className="title-navbar">CodeAMZ</h2>
-      </div>
-      <div className="index">
-        hola
-      </div>
-      <div className="homeinfo">
-        <Home />
-      </div>
-      <Outlet />
-    </div>
-  );
+    const avatarSrc = user && user.img ? user.img : '';  
+    const rolUser = user && user.rol ? user.rol : "value"
+    return (
+        <div className="navbar-container">
+            <div className="Navbar">
+                <img className="logo" src="/src/img/logo.png" alt="" />
+                <h2 className="title-navbar">CodeAMZ</h2>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild className="open-drop">
+                        <button className="boton-open" variant="outline">
+                            <img className="open-listado" src="/src/img/listado.png" alt="" />
+                        </button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <User />
+                                <span>Perfil</span>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Settings />
+                                <span>Configuracion</span>
+                            </DropdownMenuItem>
+                            
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <LogOut/>
+                                <span>Cerrar sesión</span>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <Avatar className="Avatar">
+                    <AvatarImage src={avatarSrc} alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            
+            <div className="index">
+            <DropdownMenuSeparator className="separator"/>
+                <ul>
+                    <li className="presentation-item">
+                        <House/>
+                        <button className="button-index">Home</button>
+                    </li>
+                    <DropdownMenuSeparator className="separator"/>
+                    <li className="presentation-item">
+                        <Presentation/>
+                        {rolUser=="docente" ? (
+                            <button className="button-index">Mis Clases</button>
+                        ) : (
+                            <button className="button-index">Clases</button>
+                        )}
+                    </li>
+                    <DropdownMenuSeparator className="separator"/>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div className="homeinfo">
+                <Home />
+            </div>
+            <Outlet />
+        </div>
+    );
 }
 
 export default Navbar;
