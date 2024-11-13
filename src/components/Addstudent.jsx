11b1fddb3c62055c632cb8aae29025d3ca3126c5
RafaelCastrillonEndo0 <castrillonrafael7@gmail.com>
2024-11-13 18:52:17 -0500

    Terminaod Add persona

--- src/components/Addstudent.jsx
+++ src/components/Addstudent.jsx
@@ -19,83 +19,116 @@ import {
 import { collection, doc, getDoc, getDocs, updateDoc, arrayUnion, query, where } from "firebase/firestore";
 export function Addstudent({userDetails}) {
   const username = userDetails && userDetails.username ? userDetails.username : "null"
-  const [className, setClassName] = React.useState(""); 
+  const [nameClas, setClassName] = React.useState(""); 
+  const [correo, setCorreo] = React.useState(""); 
   const [mensaje, setMensaje] = React.useState("")
   const [ok, setOk] = React.useState(false);
   const [alertType, setAlertType] = React.useState("");
-  
-  const handleInputChange = (e) => {
-    setClassName(e.target.value); 
-  };
+  const [clases, setClases] = useState([]);
+  useEffect(()=>{
+    const fetchClases = async ()=>{
+        if(userDetails && userDetails.classes && userDetails.classes.length>0){
+            try{
+                const classQuery = query(
+                    collection(fireStore, "classes"),
+                    where("profesor","==",username),
+                    where("state","==",true)
+                )
+                const querySpanshot  = await getDocs(classQuery);
+                const clasesData = querySpanshot.docs.map(doc => ({
+                    id:doc.id,
+                    ...doc.data(),
+                }));
 
-  const createClass = async () => {
-    
-    if(className==""){
-      setAlertType("info");
-      setOk(true)
-      setMensaje("Ingrese un nombre de clase valido");
-      return;
+                setClases(clasesData)
+            }catch(error){
+            }
+        }
     }
+    fetchClases();
+  }, [userDetails]);
+
+  const handleAddEstudiante = async () => {
     try {
-      const classCollection = collection(fireStore, "classes"); 
+        const classRef = query(
+            collection(fireStore, "usuarios"),
+            where("correo", "==", correo)
+        );
+        const userSnapshot = await getDocs(classRef);
 
-      const docRef = await addDoc(classCollection, {
-        name: className, 
-        createdAt: new Date(),
-        profesor: username,
-        state:true,
-      });
+        if (!userSnapshot.empty) {
+            const userId = userSnapshot.docs[0].id;
 
-      const auth = getAuth();
-      const user = auth.currentUser;
-      if (user) {
-        const userDocRef = doc(fireStore, "usuarios", user.uid);
-        await updateDoc(userDocRef, {
-          classes: arrayUnion(docRef.id),
-          state: true
-        });
-        setMensaje(`Clase creada con exito`);
-        setAlertType("success");
-        setOk(true);
-      } else {
-        setMensaje("No hay un usuario autenticado.");
-        setAlertType("warning");
-        setOk(true);
-      }
-    } catch (e) {
-      setAlertType("error")
-      setMensaje(`Error al crear la clase: ${e.message}`);
-      setOk(true); 
+            await updateDoc(doc(fireStore, "usuarios", userId), {
+                Clases_Activas: arrayUnion(nameClas)
+            });
+        } else {
+        }
+    } catch (error) {
+        console.error("Error al agregar la clase al estudiante:", error);
     }
-    
+
+    try{
+        const stRef= query(
+            collection(fireStore,"classes"),
+            where("name","==",nameClas)
+        );
+        const classSnapshot = await getDocs(stRef);
+        if(!classSnapshot.empty){
+            const classId = classSnapshot.docs[0].id;
+            await updateDoc(doc(fireStore,"classes",classId),{
+                Estudiantes_Activos: arrayUnion(correo)
+            });
+            setAlertType("success")
+            setMensaje("Estudiante agregado exitosamente.")
+            console.log("Clase agregada exitosamente al usuario.")
+        }else{
+            setAlertType("error")
+            setMensaje("Usuario no encontrado con ese correo.")
+
+        }
+    }catch(error){
+        setMensaje("No se pudo agregar el estudiante.")
+        setAlertType("error")
+    }
+    setOk(true);
+    setCorreo("")
     setClassName("")
-  };
+    console.log(correo)
+};
+ 
 
   return (
     <>
       <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>Agregar Estudiante</CardTitle>
+        <CardDescription>Complete los datos para agregar el estudiante</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="name">Correo Del Estudiante</Label>
+              <Input value={correo} onChange={(e) => setCorreo(e.target.value)} id="name" placeholder="Correo Del Estudiante" />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
+              <Label htmlFor="framework">Seleccione una Clase</Label>
+              <Select
+              value={nameClas}
+              onValueChange={(value) => setClassName(value)}
+              >
                 <SelectTrigger id="framework">
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
                 <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                {clases.length > 0 ? (
+                    clases.map((clase) => (
+                        <SelectItem key={clase.id} value={clase.name}>{clase.name}</SelectItem>
+                ))
+                ) : (
+                     <p>No tienes clases activas.</p>
+                )}
                 </SelectContent>
               </Select>
             </div>
@@ -104,8 +137,15 @@ export function Addstudent({userDetails}) {
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+        <Button onClick={handleAddEstudiante}>Agregar</Button>
       </CardFooter>
+        {ok && (
+                <div className="mt-4 transition-transform transform duration-500 ease-in-out translate-y-4">
+                <Alert severity={alertType} onClose={() => setOk(false)}>
+                    {mensaje}
+                </Alert>
+                </div>
+        )}
     </Card>
     </>
   );
